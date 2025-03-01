069639a714b8933a1a8d9863f2348c51bdbd1b87
Densho <denj.rey.c@gmail.com>
2025-03-01 06:11:14 -0500

    docs: Added documentation

--- app/src/main/java/com/audiobooks/podcasts/ui/PodcastUIState.kt
+++ app/src/main/java/com/audiobooks/podcasts/ui/PodcastUIState.kt
@@ -2,6 +2,13 @@ package com.audiobooks.podcasts.ui
 
 import com.audiobooks.podcasts.model.Podcast
 
+/**
+ * Data class representing the UI state for the podcast list.
+ *
+ * @param isLoading Boolean flag indicating if podcasts are being loaded.
+ * @param podcasts List of podcasts.
+ * @param error Error message if loading fails.
+ */
 data class PodcastUIState (
     val isLoading: Boolean = false,
     val podcasts: List<Podcast> = emptyList(),
